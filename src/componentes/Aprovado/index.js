8a86bd23e62c62a55775d18757587d66872fa05b
vitorfail <vitor_andrademanoel@hotmail.com>
2024-02-08 16:30:26 -0300

    adicional de icones

--- src/componentes/Aprovado/index.js
+++ src/componentes/Aprovado/index.js
@@ -3,17 +3,25 @@ import Re from "./aprovado.png"
 
 import './index.css'
 export default function Aprovado(props){
+    const [inicio, setinicio] = useState(true)
+    useEffect(() => {
+        setTimeout(()=> {
+            setinicio(false)
+        },4000)
+    })
     return(
         <div style={{display:props.show, width:props.largura+"px",height:props.largura+"px"}}  className="negado">
             <div className='chuva'>
-                <i style={{"--i":15}}>$</i>
-                <i style={{"--i":11}}>$</i>
-                <i style={{"--i":13}}>$</i>
-                <i style={{"--i":17}}>$</i>
-                <i style={{"--i":10}}>$</i>
-                <i style={{"--i":12}}>$</i>
-                <i style={{"--i":7}}>$</i>
-                <i style={{"--i":8}}>$</i>
+                <div className={inicio?"lista show":"lista"} >
+                    <i style={{"--i":15}}>$</i>
+                    <i style={{"--i":11}}>!$</i>
+                    <i style={{"--i":13}}>$</i>
+                    <i style={{"--i":17}}>💰</i>
+                    <i style={{"--i":10}}></i>
+                    <i style={{"--i":12}}>R$</i>
+                    <i style={{"--i":7}}>$</i>
+                    <i style={{"--i":8}}>$</i>
+                </div>
                 <img src={Re}></img>
 
             </div>
--- src/componentes/Negado/index.js
+++ src/componentes/Negado/index.js
@@ -1,19 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Re from "./reprovado.png"
 
 import './index.css'
 export default function Negado(props){
+    const [inicio, setinicio] = useState(true)
+    useEffect(() => {
+        setTimeout(()=> {
+            setinicio(false)
+        },4000)
+    })
     return(
         <div style={{display:props.show, width:props.largura+"px",height:props.largura+"px"}}  className="negado">
             <div className='chuva'>
-                <i style={{"--i":15}}>x</i>
-                <i style={{"--i":11}}>x</i>
-                <i style={{"--i":13}}>x</i>
-                <i style={{"--i":17}}>x</i>
-                <i style={{"--i":10}}>x</i>
-                <i style={{"--i":12}}>x</i>
-                <i style={{"--i":7}}>x</i>
-                <i style={{"--i":8}}>x</i>
+                <div className={inicio?"lista show":"lista"} >
+                    <i style={{"--i":15}}>x</i>
+                    <i style={{"--i":11}}>!</i>
+                    <i style={{"--i":13}}>x</i>
+                    <i style={{"--i":17}}>x</i>
+                    <i style={{"--i":10}}>!</i>
+                    <i style={{"--i":12}}>?</i>
+                    <i style={{"--i":7}}>x</i>
+                    <i style={{"--i":8}}>!</i>
+                </div>
                 <img src={Re}></img>
 
             </div>
